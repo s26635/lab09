423b43a9ddfe383c120bd00e9578a5b74b67077e
s26635 <s26635@pjwstk.edu.pl>
2024-05-20 10:42:15 +0200

    Commit

--- lab09/obj/Debug/net8.0/lab09.AssemblyInfo.cs
+++ lab09/obj/Debug/net8.0/lab09.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1cbf72e06d1037e37e97f7f7d0f058156802167")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
